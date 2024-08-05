0659b397a9a9a125633eae59eec80d6a3031dbb0
Hassan Minhas <hsnminhas@gmail.com>
2024-08-05 23:55:14 +0500

    minor change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   swcMinify: true,
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
